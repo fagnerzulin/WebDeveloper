ea04044d248ccae5b0e36292e52e0df1ac5011f0
Fagner Alexandre Zulin <fagnerzulin11@gmail.com>
2020-06-27 11:42:16 -0300

    Função que deixa o tamanho da mosca aleatória

--- JavaScript/app_game_mata_mosquito/js/jogo.js
+++ JavaScript/app_game_mata_mosquito/js/jogo.js
@@ -17,9 +17,25 @@ function posicaoRandomica() {
     //criar o elemento html
     var mosca = document.createElement("img");
     mosca.src = "img/mosca.png";
-    mosca.className = "mosca1";
+    mosca.className = tamanhoAleatorio();
     mosca.style.left = posicaoX + "px";
     mosca.style.top = posicaoY + "px";
     mosca.style.position = "absolute";
+
     document.body.appendChild(mosca);
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3);
+
+    switch (classe) {
+        case 0:
+            return "mosca1";
+
+        case 1:
+            return "mosca2";
+
+        case 2:
+            return "mosca3";
+    }
 }
